3d7f99d1bd3ec59792b2139adf6a8ddc5c7113ec
soonki-98 <alstnsrl98@gmail.com>
2026-04-27 14:16:10 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
   plugins: [react()],
+  base: repoName ? `/${repoName}/` : '/',
 })
